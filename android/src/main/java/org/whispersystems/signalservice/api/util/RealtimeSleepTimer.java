2d6de336d060cdad30f36a494a810122eabc8ea8
Jens Mueller <tschenser@gmx.de>
2019-04-13 08:04:15 +0200

    fixing RealtimeSleepTimer to work in concurrent threads

--- android/src/main/java/org/whispersystems/signalservice/api/util/RealtimeSleepTimer.java
+++ android/src/main/java/org/whispersystems/signalservice/api/util/RealtimeSleepTimer.java
@@ -10,10 +10,6 @@ import android.os.Build;
 import android.os.SystemClock;
 import android.util.Log;
 
-import org.whispersystems.signalservice.api.util.SleepTimer;
-
-import java.util.concurrent.TimeUnit;
-
 /**
  * A sleep timer that is based on elapsed realtime, so
  * that it works properly, even in low-power sleep modes.
@@ -22,21 +18,21 @@ import java.util.concurrent.TimeUnit;
 public class RealtimeSleepTimer implements SleepTimer {
   private static final String TAG = RealtimeSleepTimer.class.getSimpleName();
 
-  private final AlarmReceiver alarmReceiver;
   private final Context context;
 
   public RealtimeSleepTimer(Context context) {
     this.context = context;
-    alarmReceiver = new RealtimeSleepTimer.AlarmReceiver();
   }
 
   @Override
   public void sleep(long millis) {
+    final AlarmReceiver alarmReceiver = new RealtimeSleepTimer.AlarmReceiver();
+    final String action = AlarmReceiver.WAKE_UP_THREAD_ACTION + "." + Thread.currentThread().getId();
     context.registerReceiver(alarmReceiver,
-                             new IntentFilter(AlarmReceiver.WAKE_UP_THREAD_ACTION));
+                             new IntentFilter(action));
 
     final long startTime = System.currentTimeMillis();
-    alarmReceiver.setAlarm(millis);
+    alarmReceiver.setAlarm(millis, action);
 
     while (System.currentTimeMillis() - startTime < millis) {
         try {
@@ -54,8 +50,8 @@ public class RealtimeSleepTimer implements SleepTimer {
   private class AlarmReceiver extends BroadcastReceiver {
     private static final String WAKE_UP_THREAD_ACTION = "org.whispersystems.signalservice.api.util.RealtimeSleepTimer.AlarmReceiver.WAKE_UP_THREAD";
 
-    private void setAlarm(long millis) {
-      final Intent        intent        = new Intent(WAKE_UP_THREAD_ACTION);
+    private void setAlarm(long millis, String action) {
+      final Intent        intent        = new Intent(action);
       final PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
       final AlarmManager  alarmManager  = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
 
